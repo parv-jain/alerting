996b2bdfc3fcba0d8409c58c428b86c6032b68db
Parv Jain <parv.jain@pluang.com>
2024-11-30 13:38:30 +0530

    logger update

--- src/toolbox/logging/index.ts
+++ src/toolbox/logging/index.ts
@@ -1,4 +1,4 @@
-import Logger from 'bunyan';
+import Logger, { stdSerializers } from 'bunyan';
 
 export class Logging {
     private name: string;
@@ -11,6 +11,30 @@ export class Logging {
     }
 
     public createLogger(): Logger {
-        return Logger.createLogger({ name: this.name, tags: this.tags });
+        return Logger.createLogger({
+            name: this.name,
+            tags: this.tags,
+            serializers: {
+                ...stdSerializers,
+            },
+            streams: [
+                {
+                    level: Logger.levelFromName.info,
+                    stream: {
+                        write: (record: unknown) => {
+                            // Parse the record if it's a string
+                            const log = typeof record === 'string' ? JSON.parse(record) : record;
+
+                            // Map level number to string
+                            log.level = Logger.nameFromLevel[log.level];
+
+                            // Write the transformed log back to stdout
+                            process.stdout.write(JSON.stringify(log) + '\n');
+                        },
+                    },
+                    type: 'raw', // Keep the raw log object
+                },
+            ],
+        });
     }
 }
